2460d79c8b544f8b9ac5d8eafa914b9da000fa74
Thehrz <thehrz@outlook.com>
2022-12-24 16:47:13 +0800

    Initial commit

---
+++ pages/game.vue
@@ -0,0 +1,9 @@
+<script setup>
+  
+</script>
+
+<template>
+  <Game />
+  <!-- <nuxt-picture src="/tables/default.png" fit="fill"/> -->
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- <section> -->
+    <h1>Test AB</h1>
+  <!-- </section> -->
+</template>
