b5705db572bab398aa8e46450533a18a0b315ae4
Thehrz <thehrz@outlook.com>
2022-12-26 22:12:45 +0800

    Update head in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=0.3, minimum-scale=0.3",
+      title: "genius-invokation-simulator",
+      meta: [{ name: "description", content: "genius-invokation-simulator" }],
+    },
+  },
   modules: ["@nuxt/image-edge"],
 })
