8df9d83fa8b05c8a87563b8d60a964cb922e1063
Thehrz <thehrz@outlook.com>
2022-12-27 19:40:22 +0800

    Add font HYWenHei

--- app.vue
+++ app.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <NuxtPage />
 </template>
 
 <style>
-body {
+body,
+html {
   margin: 0;
+}
+
+img {
+  display: block;
+}
+
+@font-face {
+  font-family: 'HYWenHei';
+  src: url('/fonts/HYWenHei.woff') format('woff');
 }
 </style>
