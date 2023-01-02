722b46ac35c28ef47995d9c1f354fc0f9f024216
Thehrz <thehrz@outlook.com>
2023-01-02 20:53:04 +0800

    Update IPlayer

--- common/interfaces/IPlayer.ts
+++ common/interfaces/IPlayer.ts
@@ -1,12 +1,13 @@
 import Dice from "~~/common/enums/Dice"
-import IAction from "../IAction"
-import ICharacter from "../ICharacter"
+import IAction from "./IAction"
+import ICharacter from "./ICharacter"
 
 interface IPlayer {
   name: string
   characters: ICharacter[]
   actions: IAction[]
   dices: Dice[]
+  thisRound: boolean
 }
 
 export default IPlayer
