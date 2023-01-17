985a192395e52dc2d2b9595019650bcfec2971d5
Thehrz <71623682+Thehrz@users.noreply.github.com>
2023-01-17 04:06:58 +0000

    Add SumonInfo

--- components/SummonCard.vue
+++ components/SummonCard.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
-import ISummon from "~~/common/interfaces/ISummon";
+import ISummon from "~~/common/interfaces/ISummon"
 
 defineProps<{
   summon: ISummon
 }>()
+
+const selected = ref(false)
 </script>
 
 <template>
-  <div class="summonCard">
+  <div class="summonCard" @click="selected = true">
     <div>
       <nuxt-img :src="`/images/cards/${summon.cardID}.png`" />
     </div>
     <div class="time font_shadow" v-if="summon.time">
       {{ summon.time }}
     </div>
+
+    <transition name="fade">
+      <nuxt-img
+        v-if="selected"
+        class="selectd"
+        src="images/icons/card_select.png"
+      />
+    </transition>
+
+    <summon-info :show="selected" :summon="summon" @close="selected = false" />
   </div>
 </template>
 
@@ -47,6 +59,14 @@ defineProps<{
 }
 .time {
   background-image: url("/images/icons/time.png");
+}
+
+.selectd {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90%;
 }
 
 img {
