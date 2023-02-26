ea7c3703c4af476c07477d68236b93806ed64648
Thehrz <thehrz@outlook.com>
2023-02-26 13:42:21 +0800

    Update vite & ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite"
+import path from "path"
 import Vue from "@vitejs/plugin-vue"
 import Pages from "vite-plugin-pages"
 import AutoImport from "unplugin-auto-import/vite"
@@ -8,6 +9,11 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
   plugins: [
     AutoImport({
       imports: ["vue", "vue-router", "@vueuse/core"],
