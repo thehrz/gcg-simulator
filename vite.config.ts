e83575d9069d8c8cf68024ca80338cb105a56a71
Thehrz <thehrz@outlook.com>
2023-03-02 22:28:53 +0800

    Add mock

--- vite.config.ts
+++ vite.config.ts
@@ -6,33 +6,41 @@ import AutoImport from "unplugin-auto-import/vite"
 import Layouts from "vite-plugin-vue-layouts"
 import Components from "unplugin-vue-components/vite"
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import { viteMockServe as ViteMockServe } from "vite-plugin-mock"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "~/": `${path.resolve(__dirname, "src")}/`,
+export default defineConfig(({ command }) => {
+  return {
+    resolve: {
+      alias: {
+        "~/": `${path.resolve(__dirname, "src")}/`,
+      },
     },
-  },
-  plugins: [
-    AutoImport({
-      imports: ["vue", "vue-router", "@vueuse/core"],
-      dts: "src/auto-imports.d.ts",
-      dirs: ["src/common/**", "src/utils/**"],
-      vueTemplate: true,
-      resolvers: [ElementPlusResolver()],
-    }),
+    plugins: [
+      AutoImport({
+        imports: ["vue", "vue-router", "@vueuse/core"],
+        dts: "src/auto-imports.d.ts",
+        dirs: ["src/common/**", "src/utils/**"],
+        vueTemplate: true,
+        resolvers: [ElementPlusResolver()],
+      }),
 
-    Components({
-      dts: "src/components.d.ts",
-      dirs: ["src/components"],
-      resolvers: [ElementPlusResolver()],
-    }),
+      Components({
+        dts: "src/components.d.ts",
+        dirs: ["src/components"],
+        resolvers: [ElementPlusResolver()],
+      }),
 
-    Pages(),
+      Pages(),
 
-    Layouts(),
+      Layouts(),
 
-    Vue(),
-  ],
+      ViteMockServe({
+        mockPath: "mock",
+        localEnabled: command === "serve",
+      }),
+
+      Vue(),
+    ],
+  }
 })
