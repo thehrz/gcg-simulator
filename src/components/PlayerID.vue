23b262174c54a06291b4f3a03a1a1ab6a937dd2b
Thehrz <thehrz@outlook.com>
2023-03-16 23:01:25 +0800

    Update PlayerID

--- src/components/PlayerID.vue
+++ src/components/PlayerID.vue
@@ -23,42 +23,42 @@ defineProps<{
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .playerid {
   display: flex;
   align-items: center;
   margin: 2%;
   position: relative;
-}
 
-.avatar {
-  position: absolute;
-  width: 29.65%;
-  left: 10.8%;
-  top: 7%;
-  clip-path: circle(50%);
-}
+  .thisround {
+    position: relative;
+    width: 10%;
+    left: 2%;
+    margin: -5%;
+  }
 
-.id {
-  width: 100%;
-}
+  .avatar {
+    position: absolute;
+    width: 29.65%;
+    left: 10.8%;
+    top: 7%;
+    clip-path: circle(50%);
+  }
 
-.thisround {
-  position: relative;
-  width: 10%;
-  left: 2%;
-  margin: -5%;
-}
+  .id {
+    width: 100%;
+  }
 
-.name {
-  position: absolute;
-  top: 6%;
-  left: 42.5%;
-  height: 0;
-  width: 50%;
-  padding: 8% 0;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
+  .name {
+    position: absolute;
+    top: 6%;
+    left: 42.5%;
+    height: 0;
+    width: 50%;
+    padding: 8% 0;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
 }
 </style>
