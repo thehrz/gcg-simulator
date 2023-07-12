4f217d02f771b8dc0291aa39f7cf9225d46ee1c5
thehrz <thehrz@outlook.com>
2023-07-12 10:08:22 +0800

    refactor(DicesNumber): update styles

--- src/components/DicesNumber.vue
+++ src/components/DicesNumber.vue
@@ -5,22 +5,8 @@ defineProps<{ num: number }>()
 <template>
   <svg viewBox="0 0 110 110">
     <g>
-      <circle
-        cx="55"
-        cy="55"
-        r="50"
-        fill="none"
-        stroke="#d3b879"
-        stroke-width="5"
-      />
-      <text
-        x="55"
-        y="75"
-        text-anchor="middle"
-        fill="white"
-        class="font"
-        style="font-size: 60px;"
-      >
+      <circle cx="55" cy="55" r="50" fill="none" stroke="#d3b879" stroke-width="5" />
+      <text x="55" y="75" text-anchor="middle" fill="white" class="font text-60px">
         {{ num }}
       </text>
     </g>
