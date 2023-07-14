14a6cac6bc81e738e92e647b817554d2f6b288b1
thehrz <thehrz@outlook.com>
2023-07-14 21:51:42 +0800

    refactor(CharacterCard): update styles

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -13,105 +13,42 @@ const card = getCharacter(cardID)
 </script>
 
 <template>
-  <div class="card" @click="selected = true">
-    <div class="elementIcons" v-if="state">
-      <ElementIcon v-for="element in state" :element="element" />
+  <div class="card m-4%" @click="selected = true">
+    <div class="elementIcons flex justify-center" v-if="state">
+      <ElementIcon class="w-25%" v-for="element in state" :element="element" />
     </div>
 
-    <div class="cardBoard">
-      <img :src="getCard(cardID)" />
-      <div class="hp font_shadow">{{ hp }}</div>
-      <div class="actionIcons">
-        <img :src="getIcon('weapon')" v-if="weapon" />
-        <img :src="getIcon('artifact')" v-if="artifact" />
-        <img :src="getIcon('talent')" v-if="talent" />
+    <div class="cardBoard bg-fill p-3% relative cursor-pointer">
+      <img class="w-100%" :src="getCard(cardID)" />
+      <div class="hp font_shadow flex items-center justify-center py-20.5% h-0 w-34% bg-fill absolute top--8% left--12%">
+        {{ hp }}</div>
+      <div class="flex flex-col items-center absolute w-28% left--9% top-20%">
+        <img class="w-100%" :src="getIcon('weapon')" v-if="weapon" />
+        <img class="w-100%" :src="getIcon('artifact')" v-if="artifact" />
+        <img class="w-100%" :src="getIcon('talent')" v-if="talent" />
       </div>
-      <div class="energyIcons">
-        <EnergyIcon :is-full="index <= energy" v-for="index in card.energy" />
+      <div class="energyIcons flex flex-col items-center w-35% absolute right--16% top-3%">
+        <EnergyIcon class="mb--30%" :is-full="index <= energy" v-for="index in card.energy" />
       </div>
       <transition name="fade">
         <selected-icon :selected="selected" />
       </transition>
     </div>
 
-    <character-info
-      :show="selected"
-      :character="character"
-      @close="selected = false"
-    />
+    <character-info :show="selected" :character="character" @close="selected = false" />
   </div>
 </template>
 
 <style scoped lang="scss">
-.card {
-  margin: 4%;
+.cardBoard {
+  background-image: url("/images/icons/card_border.png");
+}
 
-  .elementIcons {
-    display: flex;
-    justify-content: center;
+.hp {
+  background-image: url("/images/icons/hp.png");
+}
 
-    > img {
-      width: 25%;
-    }
-  }
-
-  .cardBoard {
-    background: url("/images/icons/card_border.png");
-    background-size: 100% 100%;
-    padding: 3%;
-    position: relative;
-    cursor: pointer;
-
-    > img:first-child {
-      width: 100%;
-    }
-
-    .hp {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      padding: 20.5% 0;
-      height: 0;
-      width: 34%;
-      background-image: url("/images/icons/hp.png");
-      background-size: 100% 100%;
-      position: absolute;
-      top: -8%;
-      left: -12%;
-    }
-
-    .actionIcons {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      position: absolute;
-      width: 28%;
-      left: -9%;
-      top: 20%;
-
-      > img {
-        width: 100%;
-      }
-    }
-
-    .energyIcons {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      width: 35%;
-      position: absolute;
-      right: -16%;
-      top: 3%;
-
-      > div {
-        width: 100%;
-        margin-bottom: -30%;
-      }
-
-      .null {
-        padding: 15%;
-      }
-    }
-  }
+.null {
+  padding: 15%;
 }
 </style>
